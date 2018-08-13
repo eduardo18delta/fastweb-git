835c9f205c0921898ecdce0efb2d14f6f9961312
Eduardo18delta <eduardo18delta@gmail.com>
2018-08-13 11:32:56 -0300

    modificando a senha do arquivo init.php

--- administrator/content/init.php
+++ administrator/content/init.php
@@ -1,7 +1,7 @@
 
 
 <?php
-$pdo = new PDO(   'mysql:host=localhost; dbname=supermercado', 'root', 'drefj508', 
+$pdo = new PDO(   'mysql:host=localhost; dbname=supermercado', 'root', '', 
             array(
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
             )
