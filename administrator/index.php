52bc0927f78150791be705b7bbd7a3905601d1ec
Eduardo18delta <eduardo18delta@gmail.com>
2018-08-13 13:48:02 -0300

    Mudando o texto o texto do botão

--- administrator/index.php
+++ administrator/index.php
@@ -32,7 +32,7 @@
 <a class="link" href="#">Esqueceu sua senha?</a>
 
 <input class="log-btn" type="submit" value="Login"><br><br>
-<a class="log-btn" href="http://127.0.0.1/fastweb-git/supermercado/">Home</a> 
+<a class="log-btn" href="http://127.0.0.1/fastweb-git/supermercado/">Ir à loja</a> 
 
 </div>
 </form>
