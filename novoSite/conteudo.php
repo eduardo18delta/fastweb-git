517bfaa2ff61114433c7dbb6d25b32680662286a
Brenno Farias <brennohenriqueskt@gmail.com>
2018-10-25 00:27:48 -0300

    Desenvolvendo conteudo.php

---
+++ novoSite/conteudo.php
@@ -0,0 +1,301 @@
+<?php
+  include 'links.php';
+?>
+<style>
+  h4{
+    color:rgb(185, 29, 29);
+  }
+  .bordaCima{
+    border-top:1px solid #000;
+  }
+  .bor{
+    border:1px solid #000;
+  }
+  .btnMais{
+    margin-left:152px;
+    font-weight:bolder;
+  }
+  .prod{
+    height:220px;
+    border:1px solid #000;
+  }
+  .fotoProd{
+    /* border:1px solid red; */
+    width:60%;
+    height:100%;
+    float:left;
+  }
+  .fotoProd img{
+    width:100%;
+    height:100%;
+  }
+  .infoProd{
+    width:40%;
+    height:100%;
+    /* border:1px solid blue; */
+    float:right;
+    position:relative;
+  }
+  .btnComprar{
+    position:absolute;
+    bottom:0;
+    left:15%;
+  }
+</style>
+<script>
+
+</script>
+<main>
+  <!--============PROMOÇÃO============-->
+  <section class="d-flex flex-column align-items-center my-5">
+    <h4>Produtos em promoção</h4>
+    <h6>Aqui tem! Compre antes que acabe!</h6>
+    <div class="d-flex flex-wrap justify-content-center bordaCima">
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+
+      <div class="col-3 m-2 prod">
+        <div class="fotoProd">
+          <img src="_img/home/prod1.jpg" />
+        </div>
+        <div class=" d-flex flex-column align-items-center infoProd">
+          <p>Calabresa</p>
+          <div class="precoProd">
+            R$ 10,00
+          </div>
+          <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+        </div>
+      </div>
+    </div>
+    <input type="submit" class="btn btn-warning align-self-start btnMais" value="Clique aqui e veja mais produtos"/>
+  </section>
+<!--=========FIM PROMOÇÃO=============-->
+<!--=========MAIS VENDIDOS============-->
+<section class="d-flex flex-column align-items-center my-5">
+  <h4>Produtos mais vendidos</h4>
+  <h6>Corra e garanta o seu!</h6>
+  <div class="d-flex flex-wrap justify-content-center bordaCima">
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+
+    <div class="col-3 m-2 prod">
+      <div class="fotoProd">
+        <img src="_img/home/prod1.jpg" />
+      </div>
+      <div class=" d-flex flex-column align-items-center infoProd">
+        <p>Calabresa</p>
+        <div class="precoProd">
+          R$ 10,00
+        </div>
+        <input type="button" class="btn btn-danger btnComprar" value="Comprar"/>
+      </div>
+    </div>
+  </div>
+  <input type="submit" class="btn btn-warning align-self-start btnMais" value="Clique aqui e veja mais produtos"/>
+</section>
+<!--==========FIM MAIS VENDIDOS==========-->
+</main>
