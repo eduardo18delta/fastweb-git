56b43b8d3317fb32c16bc0c12918ad83b59e6e54
eduardo18delta <eduardo18delta@gmail.com>
2018-11-21 00:55:37 -0300

    algumas modificações

--- administrator/content/init.php
+++ administrator/content/init.php
@@ -1,7 +1,7 @@
 
 
 <?php
-$pdo = new PDO(   'mysql:host=localhost; dbname=supermercado', 'root', '', 
+$pdo = new PDO(   'mysql:host=localhost; dbname=fastweb-git', 'eduardo', '123456', 
             array(
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
             )
--- administrator/login/init.php
+++ administrator/login/init.php
@@ -2,9 +2,9 @@
 
 // constantes com as credenciais de acesso ao banco MySQL
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'supermercado');
+define('DB_USER', 'eduardo');
+define('DB_PASS', '123456');
+define('DB_NAME', 'fastweb-git');
 
 // habilita todas as exibições de erros
 ini_set('display_errors', true);
